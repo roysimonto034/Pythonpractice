c20cc0dd9316ab08a90313c29773e0d63eee5790
Simonto <simontoroy1989@gmail.com>
2025-04-12 15:11:06 +0530

    added new code

--- pandas_testfile.py
+++ pandas_testfile.py
@@ -2,3 +2,10 @@
 import pandas as pd
 fp=pd.read_csv(r'E:\Python_project\annual-enterprise-survey-2021-financial-year-provisional-csv.csv')
 print(fp.columns)
+i=1
+for ip,row in fp.iterrows():
+    while i<10:
+        print(row)
+        i+=1
+    break
+ 
