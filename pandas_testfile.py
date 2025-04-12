953eddb8aee86cdaf582dff6fe3f93ccf074d05a
roysimonto034 <simontoroy1989@gmail.com>
2025-04-12 15:14:29 +0530

    Update pandas_testfile.py

--- pandas_testfile.py
+++ pandas_testfile.py
@@ -1,3 +1,4 @@
+''' This file is demonstrate pandas dataframework'''
 import pandas as pd
 fp=pd.read_csv(r'E:\Python_project\annual-enterprise-survey-2021-financial-year-provisional-csv.csv')
 print(fp.columns)
